2d2da85c65be4c82643e1c4cea0eb8f5f3c23158
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-01-09 02:20:16 +0900

    描画関数を書く

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -4,7 +4,7 @@
 
 void fir::Sprite::Init()
 {
-	glGenTextures(1, &id);
+	glGenTextures(1, &texid);
 
 	std::ifstream stream(filePath, std::ios::binary);
 
@@ -18,7 +18,7 @@ void fir::Sprite::Init()
 	stream.close();
 
 	// テクスチャを登録
-	glBindTexture(GL_TEXTURE_2D, id);
+	glBindTexture(GL_TEXTURE_2D, texid);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -26,7 +26,50 @@ void fir::Sprite::Init()
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &buffer);
 }
 
-fir::Sprite::Sprite(const char * path, GLuint tid)
-	: filePath(path), id(tid)
+// スプライトの描画処理本体
+void fir::Sprite::DrawSprite()
 {
+	glBindTexture(GL_TEXTURE_2D, texid);
+
+	glEnable(GL_TEXTURE_2D);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	glDrawArrays(GL_QUADS, 0, 4);
+
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisable(GL_TEXTURE_2D);
+}
+
+void fir::Sprite::Draw(const int x, const int y)
+{
+	const GLfloat vtx[] = {
+		x, y,
+		x + width, y,
+		x + width, y + height,
+		x, y + height
+	};
+	glVertexPointer(2, GL_FLOAT, 0, vtx);
+
+	const GLfloat texuv[] = {
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f
+	};
+	glTexCoordPointer(2, GL_FLOAT, 0, texuv);
+
+	DrawSprite();
+}
+
+fir::Sprite::Sprite(const char * path)
+	: filePath(path)
+{
+	Init();
+}
+
+fir::Sprite::~Sprite()
+{
+	delete[] buffer;
 }
--- src/Sprite.h
+++ src/Sprite.h
@@ -8,17 +8,32 @@ namespace fir
 {
 	class Sprite
 	{
-		GLuint id;
-		GLubyte *buffer;
+		GLuint texid;
+		GLubyte* buffer;
 		uint32_t width, height;
 		int depth, colortype, interlacetype;
 		std::string filePath;
 
-		void Init();
+		void Init();		// ‰Šú‰»ŠÖ”
+		void DrawSprite();	// •`‰æŠÖ”‚Ì–{‘Ì
 
 	public:
-		Sprite(const char* path, GLuint tid);
+		Sprite(const char* path);
+
+		virtual ~Sprite();
+
+		// •`‰æŠÖ”
+		void Draw(const int x, const int y);
+		template <int x, int y> void Draw();
 	};
+
+	template<int x, int y>
+	inline void Sprite::Draw()
+	{
+		Draw(x, y);
+
+		DrawSprite();
+	}
 }
 
 #endif
