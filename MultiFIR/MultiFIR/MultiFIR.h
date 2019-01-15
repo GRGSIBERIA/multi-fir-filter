541f50b9737c63156883a2473f8fe5af65f9f021
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-01-16 01:25:35 +0900

    ごめん，無理

---
+++ MultiFIR/MultiFIR/MultiFIR.h
@@ -0,0 +1,32 @@
+﻿
+// MultiFIR.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'stdafx.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CMultiFIRApp:
+// このクラスの実装については、MultiFIR.cpp を参照してください
+//
+
+class CMultiFIRApp : public CWinApp
+{
+public:
+	CMultiFIRApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMultiFIRApp theApp;
